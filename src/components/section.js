afea37e8f3e3153e66231f84a014635a375d55a5
Cookieman2002 <mcl2002@outlook.dk>
2022-06-15 13:21:01 +0200

    sections fixed

--- src/components/section.js
+++ src/components/section.js
@@ -1,21 +1,21 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import { Link } from "react-router-dom";
 import { vars } from "./variables";
 import { ThemeContext } from "../contexts/context";
 import { useContext } from "react";
-import Card from "./Card";
-import Usefetch from "../hooks/Usefetch";
+
 
 const Section = ({ posts, title }) => {
   const { isSwitch } = useContext(ThemeContext);
-  const key = "PorNsbssWGT719UfIj07BezGGZ1kzBHK";
-  const { data } =
-    Usefetch(`https://api.nytimes.com/svc/topstories/v2/${posts}.json?api-key=${key}
-    `);
+  
+let topics = posts.map((topic) => topic.section)
 
-  const spreadData = [...new Set(posts.map((article) => article.section))];
-  console.log();
+let sortedTopics = topics.filter((element, index) => {
+  return topics.indexOf(element) === index
+});
+
+console.log(sortedTopics)
+  
 
   const styles = {
     postlist: css`
@@ -24,58 +24,42 @@ const Section = ({ posts, title }) => {
       align-items: flex-start;
       gap: 1em;
       color: ${isSwitch.darkmode ? vars.typo_3 : vars.primary_3};
-      margin-bottom: 50px;
+     
+      
     `,
-    post: css`
-      display: flex;
-      flex-direction: column;
-      justify-content: flex-start;
-      align-items: center;
-      gap: 10px;
-      font-size: 15px;
-      & img {
-        height: 70px;
-        width: 70px;
-        border-radius: 50%;
-      }
-      & h2 {
-        font-size: 13px;
-      }
-    `,
+  topics: css`
+  display: flex;
+  flex-direction: column;
+  & h1{
+    align-self: center;
+  }
+
+  
+  `,
+  post: css`
+  font-size: 10px;
+  `,
     title: css`
       margin-top: 10px;
       align-self: center;
     `,
   };
 
-  // const titles = [...new Set(sorted.map((article) => article.section))];  // console.log(titles);
-
-  // const searched = data.filter((el) => {
-  //   if (posts.input === "") {
-  //     return el;
-  //   } else return el.text.toLowerCase().includes(posts.input);
-  // });
-
-  return (
-    <div css={styles.postlist}>
-      <h2 css={styles.title}>{title}</h2>
-      {spreadData.map((title, index) => (
-        <div key={index} css={styles.post}>
-          <h2>{title}</h2>
-          {data.section == title && <Card items={title}  /> }
-        </div>
-      ))}
-    </div>
+  return (<div css={styles.postlist}>
+    <h1 css={styles.title}>{title}</h1>
+    {sortedTopics.map((topic, index) => (
+      <section css={styles.topics} key={index} >
+        <h1>{topic}</h1>
+        {posts.map((story, index) => story.section === topic && 
+        <div key={index} css={styles.post}> 
+          <img src={story.multimedia[0].url} alt="" />
+          <h2>{story.title}</h2>
+          <p>{story.subsection}</p>
+          <p>{story.published_date}</p>
+        </div>)}
+      </section>
+    ))}
+  </div>
   );
-};
-// const sorted = posts.sort((a, b) => {
-//   if (a.section < b.section) {
-//     return -1;
-//   }
-//   if (a.section > b.section) {
-//     return 1;
-//   }
-//   return 0;
-// });
-
+}
 export default Section;
--- src/pages/Indbox.js
+++ src/pages/Indbox.js
@@ -1,23 +1,20 @@
-import Section from "../components/Section";
+
 import Usefetch from "../hooks/Usefetch";
 import { useState } from "react";
+import Section from "../components/Section";
 const Indbox = () => {
   const key = "PorNsbssWGT719UfIj07BezGGZ1kzBHK";
   const { error, isPending, data } =
     Usefetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${key}
     `);
-  const [inputText, setInputText] = useState("");
-  let inputHandler = (e) => {
-    var lowerCase = e.target.value.toLowerCase();
-    setInputText(lowerCase);
-  };
+
   return (
     <div>
-      <input type="text" placeholder="Search here" onChange={inputHandler} />
-      <div input={inputText}>
+     
+      <div>
         {error && <div>{error}</div>}
         {isPending && <div>Loading...</div>}
-        {data && <Section posts={data} title="Inbox" />}
+        {data && <Section posts={data} title="inbox"/>}
       </div>
     </div>
   );
